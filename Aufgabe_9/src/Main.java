468b987592175f0f021dc0839a795dc48181b17c
ManuelGut-ICT-BZ <manuel.gut@ict-bz.ch>
2024-07-30 11:00:37 +0200

    add module Aufgabe_9

---
+++ Aufgabe_9/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Zahl: ");
+
+        try {
+            int zahl = Integer.parseInt(scanner.nextLine());
+            int quersumme = berechneQuersumme(zahl);
+            System.out.println("Die Quersumme von " + zahl + " ist: " + quersumme);
+
+        } catch (NumberFormatException numberFormatException) {
+            System.out.println("❌ Es sind nur Ganzzahlen erlaubt!");
+        }
+    }
+
+    static int berechneQuersumme(int zahl) {
+        int sum = 0;
+        while (zahl != 0) {
+            sum = sum + (zahl % 10);
+            zahl = zahl / 10;
+        }
+        return sum;
+    }
+}
