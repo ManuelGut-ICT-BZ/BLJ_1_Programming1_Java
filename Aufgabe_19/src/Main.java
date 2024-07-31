aca44941d5e6cee7dd9d05775c20c6b00392a701
gum <manuel.gut@ict-bz.ch>
2024-07-31 22:54:35 +0200

    add module Aufgabe_19

---
+++ Aufgabe_19/src/Main.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static int firstElement;
+    private static int secondElement;
+    private static char operator = '_';
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Make your calculation (or press Q to quit)");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                return;
+            }
+            try {
+                setUpPartsOfCalculation(input);
+                float result = calculate(firstElement, secondElement, operator);
+                System.out.println(result);
+            } catch (Exception exception) {
+                System.out.println("Error: Unerlaubte Operation");
+            }
+        }
+    }
+
+    private static void setUpPartsOfCalculation(String input) {
+        char[] chars = input.toCharArray();
+        char[] allowedOperatorArray = {'+', '-', '*', '/'};
+        List<Character> allowedOperators = new ArrayList<>();
+        for (char element : allowedOperatorArray) {
+            allowedOperators.add(element);
+        }
+        StringBuilder firstPartStringBuilder = new StringBuilder();
+        StringBuilder secondPartStringBuilder = new StringBuilder();
+        boolean isFirstPart = true;
+        for (char element : chars) {
+            if (allowedOperators.contains(element)) {
+                operator = element;
+                isFirstPart = false;
+            } else if (isFirstPart) {
+                firstPartStringBuilder.append(element);
+            } else {
+                secondPartStringBuilder.append(element);
+            }
+        }
+        firstElement = Integer.parseInt(firstPartStringBuilder.toString().trim());
+        secondElement = Integer.parseInt(secondPartStringBuilder.toString().trim());
+    }
+
+    private static float calculate(int firstElement, int secondElement, char operator) throws Exception {
+        return switch (operator) {
+            case '+' -> add(firstElement, secondElement);
+            case '-' -> subtract(firstElement, secondElement);
+            case '*' -> multiply(firstElement, secondElement);
+            case '/' -> divide(firstElement, secondElement);
+            default -> throw new Exception("Nicht erlaubter Operator");
+        };
+    }
+
+    public static float add(int firstElement, int secondElement) {
+        return firstElement + secondElement;
+    }
+
+    public static float subtract(int firstElement, int secondElement) {
+        return firstElement - secondElement;
+    }
+
+    public static float multiply(int firstElement, int secondElement) {
+        return firstElement * secondElement;
+    }
+
+    public static float divide(int firstElement, int secondElement) {
+        return (float) firstElement / secondElement;
+    }
+}
